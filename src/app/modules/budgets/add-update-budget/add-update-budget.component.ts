b47103e226b61c2c3e2e0afbf4a0f68d11d87854
jhoncastrillon9@gmail.com <jhoncastrillon9@gmail.com>
2023-11-06 17:56:11 +0100

    Add budget

--- src/app/modules/budgets/add-update-budget/add-update-budget.component.ts
+++ src/app/modules/budgets/add-update-budget/add-update-budget.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BudgetService } from '../services/budget.service';
 import { CustomerModel } from '../../customers/models/customer.Model';
 import { CustomerService } from '../../customers/services/customer.service';
-
+import { cilPencil, cilXCircle } from '@coreui/icons';
+import { IconSetService } from '@coreui/icons-angular';
 @Component({
   selector: 'app-add-update-budget',
   templateUrl: './add-update-budget.component.html',
@@ -12,100 +13,106 @@ import { CustomerService } from '../../customers/services/customer.service';
 })
 
 export class AddUpdateBudgetComponent implements OnInit {
-  budgetForm: FormGroup = new FormGroup({});
+  budgetForm: FormGroup;
   budgetId?: string;
-  currentDate:Date = new Date();
+  currentDate: Date = new Date();
   customers: CustomerModel[] = [];
   showErrors: boolean = false;
-  msjError: string ="";
+  msjError: string = "";
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private budgetService: BudgetService,
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    public iconSet: IconSetService,
   ) {
-
-  }
-
-  ngOnInit() {
     this.budgetForm = this.fb.group({
       budgetId: ['0'],
       userId: ['0', [Validators.required]],
       customerId: ['0', [Validators.required]],
       amount: ['0', [Validators.required]],
-      date:[new Date()],
+      date: [new Date()],
       budgetName: ['', [Validators.required]],
-    });    
+      budgetDetailsDto: this.fb.array([]) // Inicializa el FormArray para los detalles del presupuesto
+    });
 
+    iconSet.icons = {  cilPencil,cilXCircle };
+  }
+
+  ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.budgetId = params.get('id')!;
 
       if (this.budgetId) {
         this.budgetService.getById(this.budgetId).subscribe((budget: any) => {
-          
+          console.log(budget);
           this.budgetForm.patchValue(budget);
         });
+      }else{
+        console.log("test addBudgetDetail");
+        //this.addBudgetDetail();
       }
     });
-
 
     this.loadCustomers();
   }
 
-
-  loadCustomers(){
+  loadCustomers() {
     this.customerService.get().subscribe(customers => {
       this.customers = customers;
     });
   }
 
+  addBudgetDetail() {
+    const budgetDetailGroup = this.fb.group({
+      budgetDetailId: [0],
+      budgetId: [0],
+      description: [''],
+      quantity: [''],
+      price: ['']
+    });
+    this.budgetDetails.push(budgetDetailGroup);
+  }
+
+  removeBudgetDetail(index: number) {
+    this.budgetDetails.removeAt(index);
+  }
+
+  get budgetDetails() {
+    return this.budgetForm.get('budgetDetailsDto') as FormArray;
+  }
+
   onAddUpdateBudget() {
     if (this.budgetForm.valid) {
-    // Obtén la fecha y hora actual
-    
-    
-    // Asigna la fecha actual al campo date
-    this.budgetForm.get('date')?.setValue(this.currentDate);
- 
+      this.budgetForm.get('date')?.setValue(this.currentDate);
       const formData = this.budgetForm.value;
-      console.log(this.budgetId);
+console.log(formData);
       if (this.budgetId) {
-        console.log("Update:");
-        console.log(formData);
         this.budgetService.update(formData).subscribe(
-          (response: any) => {                 
-            this.router.navigate(['/Budgets/budgets']);
-          },
-          (error) => {
-            // Maneja errores y muestra un mensaje al usuario
-            console.error('Error al actualizar cliente', error);
-            // Puedes mostrar una alerta aquí
-          }
-        );
-      } else {
-        console.log("Created:");
-        console.log(formData);
-        this.budgetService.add(formData).subscribe(
-          (response: any) => {                    
+          (response: any) => {
             this.router.navigate(['/budgets/budgets']);
           },
           (error) => {
-            // Maneja errores y muestra un mensaje al usuario
             this.showErrors = true;
             this.msjError = error;
-            console.error('Error al crear presupuesto', error);
-            // Puedes mostrar una alerta aquí
+          }
+        );
+      } else {
+        this.budgetService.add(formData).subscribe(
+          (response: any) => {
+            this.router.navigate(['/budgets/budgets']);
+          },
+          (error) => {
+            this.showErrors = true;
+            this.msjError = error;
           }
         );
       }
     } else {
-      // El formulario no es válido, muestra un mensaje de error o realiza alguna acción adicional.
       this.showErrors = true;
-      this.msjError = "Campos Nombre de la cotización y Cliente son obligatorios"
-      console.log('El formulario no es válido. Por favor, complete los campos correctamente.');
+      this.msjError = "Campos Nombre de la cotización y Cliente son obligatorios";
     }
   }
-  
 }
