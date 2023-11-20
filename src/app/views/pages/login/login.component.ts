d21485c6ec67d0e6516daef3253a774ee7306f15
jhoncastrillon9@gmail.com <jhoncastrillon9@gmail.com>
2023-11-20 14:43:34 +0100

    Add spiner login

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Asegúrate de importar FormGroup y Validators desde '@angular/forms'
 import { AuthService } from '../../../auth/auth.service';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,10 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder, 
+    private authService: AuthService, 
+    private router: Router,
+    private spinner: NgxSpinnerService) {
     this.loginForm = fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -21,6 +25,7 @@ export class LoginComponent {
   onLogin() {
     console.log("Start login");
     if (this.loginForm.valid) {
+      this.spinner.show();
       const formData = this.loginForm.value;
       this.authService.login(formData).subscribe(
         (response: any) => {
@@ -30,18 +35,21 @@ export class LoginComponent {
           // Almacena el token y los datos del usuario en el almacenamiento local          
           localStorage.setItem('tokenData', JSON.stringify(tokenData));
           localStorage.setItem('token', token);
+          this.spinner.hide();
           // Realiza redirección o acciones adicionales si es necesario
           this.router.navigate(['/']);
         },
         (error) => {
           // Maneja errores y muestra un mensaje al usuario
           console.error('Error al iniciar sesión', error);
+          this.spinner.hide();
           // Puedes mostrar una alerta aquí
         }
       );
     } else {
       // El formulario no es válido, muestra un mensaje de error o realiza alguna acción adicional.
       console.log('El formulario no es válido. Por favor, complete los campos correctamente.');
+      this.spinner.hide();
     }
   }
 }
