d3d8e6ec25345bc730b53eeba173a14f877d0e50
jhoncastrillon9@gmail.com <jhoncastrillon9@gmail.com>
2024-10-16 23:52:35 +0200

    update

--- src/app/modules/configurations/company-config/company-config.component.ts
+++ src/app/modules/configurations/company-config/company-config.component.ts
@@ -13,19 +13,21 @@ import { ConfirmationModalComponent } from 'src/app/shared/components/reusable-m
 export class CompanyConfigComponent implements OnInit {
   @ViewChild('confirmationModal') confirmationModal!: ConfirmationModalComponent;
   isModalError: boolean = false;
-  
+
   companyForm: FormGroup;
   companyId?: string = '0';  
   selectedFile: File | null = null;
   urlImageLogo: string = '';
 
+  // Mensajes reutilizables
   private readonly successMessage: string = "¡Los datos de tu empresa han sido actualizados con éxito!";
   private readonly successTitle: string = "¡Actualización Completada!";
   private readonly errorTitle: string = "Oops, ocurrió un error.";
-  private readonly errorformTitle: string = "Oops, Faltan datos";
+  private readonly formErrorTitle: string = "Oops, Faltan datos";
   private readonly loadDataError: string = "Algo falló al obtener los datos, refresca la página F5.";  
   private readonly updateErrorMessage: string = "Algo falló al actualizar la info de tu empresa. Intenta de nuevo, que a la segunda va la vencida.";
   private readonly formInvalidMessage: string = "El formulario necesita un poco más de amor. Completa los campos correctamente y listo. 🚀";
+
   title: string = this.successTitle;
   messageModal: string = this.successMessage;
 
@@ -67,60 +69,63 @@ export class CompanyConfigComponent implements OnInit {
         }
         this.spinner.hide();
       },
-      (error: any) => {        
-        this.handleError('Error al consultar empresa', this.loadDataError, this.errorTitle);
+      (error: any) => {
+        this.handleError('Error al consultar empresa', this.loadDataError);
         this.spinner.hide();
       }
     );
   }
 
   onFileSelected(event: any) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      if (file.type.startsWith('image/')) {
-        this.selectedFile = file;
-      } else {
-        alert('Solo se pueden cargar imágenes.');
-      }
+    const file = event.target.files[0];
+    if (file && file.type.startsWith('image/')) {
+      this.selectedFile = file;
+    } else {
+      this.showModal(true, 'Solo puedes cargar archivos de tipo imagenes 😉', this.errorTitle);
     }
   }
 
   updateCompany() {
     if (this.companyForm.valid) {
       this.spinner.show();
-      const formData = new FormData();
-      formData.append('Email', this.companyForm.get('email')?.value);
-      formData.append('CompanyName', this.companyForm.get('companyName')?.value);
-      formData.append('Address', this.companyForm.get('address')?.value);
-      formData.append('CompanyId', this.companyForm.get('companyId')?.value);
-      formData.append('Document', this.companyForm.get('document')?.value);
-      formData.append('Telephones', this.companyForm.get('telephones')?.value);
-      formData.append('UrlWeb', this.companyForm.get('urlWeb')?.value);
-
-      if (this.selectedFile) {
-        formData.append('FileLogo', this.selectedFile);
-      } 
+      const formData = this.buildFormData();
 
       this.companyService.updateCompanyByUser(formData).subscribe(
         (response: any) => {
           this.ngOnInit();
           this.spinner.hide();  
-          this.showModal(false,this.successMessage,this.title);
+          this.showModal(false, this.successMessage, this.successTitle);
         },
         (error: any) => {
           this.spinner.hide();     
-          this.showModal(true,this.updateErrorMessage,this.errorTitle);
+          this.showModal(true, this.updateErrorMessage, this.errorTitle);
         }
       );
     } else {
-      this.showModal(true,this.formInvalidMessage,this.errorformTitle);
+      this.showModal(true, this.formInvalidMessage, this.formErrorTitle);
     }
   }
- 
 
-  private handleError(consoleMessage: string, messageError: string, errorTitleError: string) {
+  private buildFormData(): FormData {
+    const formData = new FormData();
+    formData.append('Email', this.companyForm.get('email')?.value);
+    formData.append('CompanyName', this.companyForm.get('companyName')?.value);
+    formData.append('Address', this.companyForm.get('address')?.value);
+    formData.append('CompanyId', this.companyForm.get('companyId')?.value);
+    formData.append('Document', this.companyForm.get('document')?.value);
+    formData.append('Telephones', this.companyForm.get('telephones')?.value);
+    formData.append('UrlWeb', this.companyForm.get('urlWeb')?.value);
+
+    if (this.selectedFile) {
+      formData.append('FileLogo', this.selectedFile);
+    }
+
+    return formData;
+  }
+
+  private handleError(consoleMessage: string, modalMessage: string) {
     console.error(consoleMessage);
-    this.showModal(true, messageError, errorTitleError);
+    this.showModal(true, modalMessage, this.errorTitle);
   }
 
   showModal(isError: boolean, message: string, title: string = this.errorTitle) {
@@ -129,5 +134,4 @@ export class CompanyConfigComponent implements OnInit {
     this.messageModal = message;
     this.confirmationModal.openModal();
   }
-
 }
